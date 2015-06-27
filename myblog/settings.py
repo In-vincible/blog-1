60f31ef9935565976220e8497c5824042f2c1a85
hustshawn <hustshawn@gmail.com>
2015-06-27 21:00:44 +0800

    I

--- myblog/settings.py
+++ myblog/settings.py
@@ -53,7 +53,6 @@ ROOT_URLCONF = 'myblog.urls'
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
-STATIC_ROOT = '/app/static'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -80,8 +79,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = '/app/static'
 STATIC_URL = '/static/'
+
 # STATICFILES_DIRS = ( 
 #     os.path.join(BASE_DIR, "static"),
 # )
