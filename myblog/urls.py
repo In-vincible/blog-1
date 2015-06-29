58f5124a2ace50577458b417f8b8c857512fad29
hustshawn <hustshawn@gmail.com>
2015-06-30 00:39:58 +0800

    Modified the static link in the base.html

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,11 +4,11 @@ from blog import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'myblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^', 'blog.views.index', name="index"),
     url(r'^', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
